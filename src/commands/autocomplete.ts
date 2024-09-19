bee3165c1f7173dbf33d6c2f0f92cab79c94e9f3
David Pine <david.pine.7@gmail.com>
2024-09-19 13:01:46 -0500

    Address append logic fix. And share WIP

--- src/commands/autocomplete.ts
+++ src/commands/autocomplete.ts
@@ -1,6 +1,11 @@
-import { CancellationToken, Position, ProviderResult, TextDocument, CompletionItemProvider, CompletionContext, CompletionItem, CompletionList, CompletionItemKind } from "vscode";
+import { CancellationToken, Position, ProviderResult, TextDocument, CompletionItemProvider, CompletionContext, CompletionItem, CompletionList, CompletionItemKind, InlineCompletionItemProvider, InlineCompletionContext, InlineCompletionItem, InlineCompletionList, Range, CodeActionProvider, CodeActionContext, Selection, CodeAction, WorkspaceEdit, CodeActionKind } from "vscode";
 import { insertXrefLinkCommandName } from '../consts';
 import { SearchOptions } from './types/SearchOptions';
+import { ApiService } from "../services/api-service";
+import { EmptySearchResults } from "./types/SearchResults";
+import { RawGitService } from "../services/raw-git-service";
+import { DocIdService } from "../services/docid-service";
+import { ItemType } from "./types/ItemType";
 
 export const xrefStarterAutoComplete: CompletionItemProvider = {
     provideCompletionItems: (
@@ -84,5 +89,126 @@ export const xrefDisplayTypeAutoComplete: CompletionItemProvider = {
         }
 
         return undefined;
+    }
+}
+
+export const xrefInlineAutoComplete: InlineCompletionItemProvider = {
+    provideInlineCompletionItems: async (
+        document: TextDocument,
+        position: Position,
+        context: InlineCompletionContext,
+        token: CancellationToken): Promise<InlineCompletionItem[] | InlineCompletionList | undefined> => {
+
+        const regexp = /[<(]xref:(.+)[>)]/;
+        if (position.line <= 0) {
+            return undefined;
+        }
+
+        const range = document.getWordRangeAtPosition(position, regexp);
+        if (!range) {
+            return undefined;
+        }
+
+        const line = document.getText(range);
+        const match = line.match(regexp);
+        const text = match?.[1];
+
+        if (text && !token.isCancellationRequested) {
+
+            const results = await ApiService.searchApi(text);
+            if (results instanceof EmptySearchResults && results.isEmpty === true) {
+                return undefined;
+            }
+
+            if (token.isCancellationRequested) {
+                console.log(`Cancellation requested after search results found.`);
+                return undefined;
+            }
+
+            const firstResult = results.results[0];
+
+            const rawUrl = await RawGitService.getRawGitUrl(firstResult.url);
+            if (!rawUrl || token.isCancellationRequested) {
+                console.log(`Cancellation requested after raw git URL found.`);
+                return undefined;
+            }
+
+            const docId = await DocIdService.getDocId(
+                firstResult.displayName, firstResult.itemType as ItemType, rawUrl);
+            if (docId && token.isCancellationRequested === false) {
+                console.log(`Added completion for ${docId}.`);
+
+                const insertText = docId.substring(text.length);
+
+                const item = new InlineCompletionItem(
+                    insertText, range);
+
+                item.filterText = docId;
+
+                return [item];
+            }
+        }
+
+        return undefined;
+    }
+}
+
+export class DisplayPropertyChanger implements CodeActionProvider {
+    provideCodeActions(
+        document: TextDocument,
+        range: Range | Selection,
+        context: CodeActionContext,
+        token: CancellationToken): ProviderResult<CodeAction[]> {
+
+        if (range.isSingleLine === false) {
+            return undefined;
+        }
+
+        const line = document.lineAt(range.start.line);
+        const text = line.text;
+        const match = text.match(/xref:.+\?displayProperty=(\w+)/);
+        if (match) {
+            const displayProperty = match[1];
+
+            return displayProperty === 'fullName'
+                ? [
+                    this.createFix(
+                        document,
+                        range,
+                        '?displayProperty=nameWithType',
+                        'Format as name with type')
+                ]
+                : [
+                    this.createFix(
+                        document,
+                        range,
+                        '?displayProperty=fullName',
+                        'Format as full name')
+                ];
+        }
+
+        return undefined;
+    }
+
+    private createFix(document: TextDocument, range: Range, newText: string, title: string): CodeAction {
+        const action = new CodeAction(title);
+        action.edit = new WorkspaceEdit();
+        const targetRange = this.getReplacementRange(document, range);
+        action.edit.replace(document.uri, targetRange, newText);
+
+        return action;
+    }
+
+    private getReplacementRange(document: TextDocument, range: Range): Range {
+        const line = document.lineAt(range.start.line);
+        const regex = /xref:.+(\?displayProperty=.+)>/;
+        const match = line.text.match(regex);
+        if (match) {
+            const start = range.start.translate(0, match.index);
+            const end = start.translate(0, match[1].length);
+            return new Range(start, end);
+        }
+
+        return range;
     }
 }
